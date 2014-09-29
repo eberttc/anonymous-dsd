1ee79f53e29cb206b9eab0b92473bb1f06974604
eberttc <eberttc@gmail.com>
2014-09-29 19:07:11 +0000

    rest service registrar odontlogo

---
+++ DentalSuite/RestService/Persistencia/ConexionUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestService.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            return "Data Source=EBERT-PC\\EBERT;Initial Catalog=DBDentalSuite;Integrated Security=SSPI;";
+        }
+
+        public static string Cadena
+        {
+            get
+            {
+                return "Data Source=EBERT-PC\\EBERT;Initial Catalog=DBDentalSuite;Integrated Security=SSPI;";
+            }
+        }
+    }
+}
---
+++ DentalSuite/RestService/Persistencia/OdontologoDAO.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestService.Dominio;
+using System.Data.SqlClient;
+
+namespace RestService.Persistencia
+{
+    public class OdontologoDAO:BaseDAO<Odontologo,string>
+    {
+
+        public Odontologo BuscarCorreoRepetido(string correo)
+        {
+
+            Odontologo OdontologoEncontrado = null;
+            string sql = "Select * from Odontologo where Correo=@Correo";
+            using (SqlConnection con = new SqlConnection(ConexionUtil.Cadena))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(sql, con))
+                {
+                    com.Parameters.Add(new SqlParameter("@Correo", correo));
+                    using (SqlDataReader resultado = com.ExecuteReader())
+                    {
+                        if (resultado.Read())
+                        {
+                            OdontologoEncontrado = new Odontologo()
+                            {
+
+
+                                Codigo = (string)resultado["Codigo"]
+
+
+                            };
+
+                        }
+
+                    }
+
+                }
+
+
+            }
+            return OdontologoEncontrado;
+
+        }
+
+        public Odontologo BuscarCOPRepetido(string cop)
+        {
+
+            Odontologo OdontologoEncontrado = null;
+            string sql = "Select * from Odontologo where COP=@COP";
+            using (SqlConnection con = new SqlConnection(ConexionUtil.Cadena))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(sql, con))
+                {
+                    com.Parameters.Add(new SqlParameter("@COP", cop));
+                    using (SqlDataReader resultado = com.ExecuteReader())
+                    {
+                        if (resultado.Read())
+                        {
+                            OdontologoEncontrado = new Odontologo()
+                            {
+
+
+                                Codigo = (string)resultado["Codigo"]
+
+
+                            };
+
+                        }
+
+                    }
+
+                }
+
+
+            }
+            return OdontologoEncontrado;
+
+        }
+    }
+}
