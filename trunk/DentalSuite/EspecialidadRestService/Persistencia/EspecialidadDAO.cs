de3753ae9047bac69fa4186e887678ede3d58de6
eberttc@gmail.com <eberttc@gmail.com@f1c9d334-9346-2773-a052-389ea6dad286>
2014-09-29 22:46:25 +0000

    ESPECIALIDAD SERVICE

---
+++ trunk/DentalSuite/EspecialidadRestService/Persistencia/EspecialidadDAO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace EspecialidadRestService.Persistencia
+{
+    public class EspecialidadDAO:BaseDAO<Dominio.Especialidad,int>
+    {
+
+        public Dominio.Especialidad BuscarNombreRepetido(string nombre)
+        {
+
+            Dominio.Especialidad EspecialidadEncontrado = null;
+            string sql = "Select * from Especialidad where Nombre=@Nom";
+            using (SqlConnection con = new SqlConnection(ConexionUtil.Cadena))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(sql, con))
+                {
+                    com.Parameters.Add(new SqlParameter("@Nom", nombre));
+                    using (SqlDataReader resultado = com.ExecuteReader())
+                    {
+                        if (resultado.Read())
+                        {
+                            EspecialidadEncontrado = new Dominio.Especialidad
+                            {
+                                Codigo = (int)resultado["Codigo"]
+
+                            };
+
+                        }
+
+                    }
+
+                }
+
+
+            }
+            return EspecialidadEncontrado;
+
+        }
+       
+    }
+}
